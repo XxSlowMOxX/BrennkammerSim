458b4af26e214398b32717734c08698b9e55620e
SlowMO_Desktop <moritz-schuetz@gmx.de>
2022-05-30 23:41:58 +0200

    Es Läuft

--- kernel.cu
+++ kernel.cu
@@ -2,8 +2,11 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+#include <iostream>
 #include <stdio.h>
 #include <fstream>
+#include <algorithm>
+#include <string>
 
 #define NOTHING 0
 #define NEW 1
@@ -11,165 +14,182 @@
 #define BURN2 3
 #define BURNT 4
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-cudaError_t simStep(int* c);
+#define CHAMBERWIDTH 10
+#define CHAMBERHEIGHT 10
 
-const int chamberWidth = 200;
-const int chamberHeight = 200;
-const int chamberArrLen = chamberHeight * chamberWidth;
+cudaError_t simStep(int* chamber, int* newchamber);
 
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
-    printf("%d", i);
-}
+const int chamberArrLen = (CHAMBERHEIGHT + 2) * (CHAMBERWIDTH + 2);
 
-__global__ void simStepKernel(int* c) {
-    int i = threadIdx.x;
-}
-
-__global__ void simKernel(int* chamber) {
+__global__ void simKernel(int* chamber, int* newchamber) {
     int x = threadIdx.x;
-    int y = threadIdx.y;
-    int cellnumber = (y * chamberWidth) + x;
+    int y = blockIdx.x;
+    int cellnumber = ((y+1) * (CHAMBERWIDTH+2)) + x + 1;
     switch (chamber[cellnumber]) {
     case NOTHING:
+        newchamber[cellnumber] = NOTHING;
         return;
         break;
     case NEW:
-        if (chamber[cellnumber + chamberWidth] == BURN2 || chamber[cellnumber + 1] == BURN2) {
-            chamber[cellnumber] = BURN1;
+        newchamber[cellnumber] = NEW;
+        if (chamber[cellnumber + CHAMBERWIDTH + 2] == BURN2 
+            || chamber[cellnumber + 1] == BURN2 
+            || chamber[cellnumber - CHAMBERWIDTH - 2] == BURN2
+            || chamber[cellnumber - 1] == BURN2) {
+            newchamber[cellnumber] = BURN1;
         }
         return;
         break;
     case BURN1:
-        chamber[cellnumber] == BURN2;
+        newchamber[cellnumber] = BURN2;
         return;
         break;
-    default:
+    default: //Wenn BURN2 oder BURNT
+        newchamber[cellnumber] = BURNT;
+        return;
         break;
     }
 }
 
 int main()
 {
-    std::ofstream img("picture.ppm");
-    img << "P3" << std::endl;
-    img << "200 200" << std::endl;
-    img << "255" << std::endl;
-
-    for (int y = 0; y < 200; y++) {
-        for (int x = 0; x < 200; x++){
-            img << "0 120 30" << std::endl;
-        }
-    }
-
-    system("cd C:\\Users\\morit\\source\\repos\\BrennkammerSim\\");
-    system("magick convert picture.ppm picture.png");
-
-
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
-
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
-
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
-
     
+    int chamber[chamberArrLen] = { 0 };
+    std::fill_n(chamber, chamberArrLen, NEW);
+    chamber[CHAMBERWIDTH * 2] = BURN1;
+
+    for (int y = 0; y < CHAMBERHEIGHT; y++) {
+        for (int x = 0; x < CHAMBERWIDTH; x++)
+        {
+            std::cout << " " << chamber[((y + 1) * (CHAMBERWIDTH+2)) + x + 1];
+        }
+        std::cout << std::endl;
+    }
+
+    cudaError_t cudaStatus;
+
+    std::string filename;
+
+    int step = 0;
+    while (step < 5) {
+        cudaStatus = simStep(chamber, 0);
+
+        filename = "Brennkammer-" + std::to_string(step) + ".ppm";
+
+        if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "simulation failed at Step %d", step);
+            return 1;
+        }
+
+        std::ofstream img(filename);
+        img << "P3\n";
+        img << std::to_string(CHAMBERHEIGHT) << " " << std::to_string(CHAMBERWIDTH) << std::endl;
+        img << "255" << std::endl;
+
+        for (int y = 0; y < CHAMBERHEIGHT; y++) {
+            for (int x = 0; x < CHAMBERWIDTH; x++)
+            {
+                switch (chamber[((y + 1) * (CHAMBERWIDTH + 2)) + x + 1]) {
+                case NOTHING:
+                    img << "255 255 255 \n";
+                    break;
+                case NEW:
+                    img << "48 0 0\n";
+                    break;
+                case BURN1:
+                    img << "166 111 2\n";
+                    break;
+                case BURN2:
+                    img << "255 106 0\n";
+                    break;
+                case BURNT:
+                    img << "0 0 0\n";
+                    break;
+                default:
+                    img << "14 237 14\n";
+                    break;
+                }
+            }
+        }
+        img.close();
+        step++;
+    }
+
     cudaStatus = cudaDeviceReset();
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaDeviceReset failed!");
         return 1;
     }
-
     return 0;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
+cudaError_t simStep(int* chamber, int* newchamber) {
+    int* dev_chamber = 0;
+    int* dev_newchamber = 0;
     cudaError_t cudaStatus;
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
+
+    cudaStatus = cudaSetDevice(0); //set GPU
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
+        cudaFree(dev_chamber);
+        cudaFree(dev_newchamber);
+        return cudaStatus;
     }
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
+#pragma region Memory Allocation
+    cudaStatus = cudaMalloc((void**)&dev_chamber, chamberArrLen * sizeof(int)); //Allocate Memory for current chamber
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
+        fprintf(stderr, "Memory Allocation Failed");
+        cudaFree(dev_chamber);
+        cudaFree(dev_newchamber);
+        return cudaStatus;
     }
 
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+    cudaStatus = cudaMalloc((void**)&dev_newchamber, chamberArrLen * sizeof(int)); //Allocate Memory for next Chamber
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
+        fprintf(stderr, "Memory Allocation Failed");
+        cudaFree(dev_chamber);
+        cudaFree(dev_newchamber);
+        return cudaStatus;
     }
 
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
+    cudaStatus = cudaMemcpy(dev_chamber, chamber, chamberArrLen * sizeof(int), cudaMemcpyHostToDevice); //copy current chamber into Device Memory
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
+        fprintf(stderr, "Memory Transfer from Host to Device Failed");
+        cudaFree(dev_chamber);
+        cudaFree(dev_newchamber);
+        return cudaStatus;
     }
 
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+#pragma endregion
 
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+    simKernel <<<CHAMBERHEIGHT, CHAMBERWIDTH >>> (dev_chamber, dev_newchamber);
 
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
     cudaStatus = cudaGetLastError();
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
+        fprintf(stderr, "Kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        cudaFree(dev_chamber);
+        cudaFree(dev_newchamber);
+        return cudaStatus;
     }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
+
     cudaStatus = cudaDeviceSynchronize();
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching Kernel!\n", cudaStatus);
+        cudaFree(dev_chamber);
+        cudaFree(dev_newchamber);
+        return cudaStatus;
     }
 
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaStatus = cudaMemcpy(chamber, dev_newchamber, chamberArrLen * sizeof(int), cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
+        fprintf(stderr, "Memory Transfer from Device to hostFailed");
+        cudaFree(dev_chamber);
+        cudaFree(dev_newchamber);
+        return cudaStatus;
     }
 
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
     return cudaStatus;
+
 }
